c8b6e5698a17b001455655425b5366f91bc64251
Matteo <matteo@fusefactory.it>
2019-03-12 21:31:30 +0100

    RayAttr

---
+++ src/Id/Components/RayAttr.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace MicroState.Id.Components
+{
+	public class RayAttr : BaseAttr<Ray>
+	{
+		[System.Serializable]
+		public class ValueTypeEvent : UnityEvent<Ray> { }
+		public ValueTypeEvent ValueEvent = new ValueTypeEvent();
+
+		override protected void OnValue(Ray v)
+		{
+			if (!(this.InvokeWhenInactive || this.isActiveAndEnabled)) return;
+			this.ValueEvent.Invoke(v);
+		}
+	}
+
+#if UNITY_EDITOR
+    [CustomEditor(typeof(RayAttr))]
+    [CanEditMultipleObjects]
+    public class RayAttrEditor : AttrRefEditor<Ray, RayAttr>
+    {
+
+    }
+#endif
+}
