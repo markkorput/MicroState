e41361090a605d011871b3ff9b4707ff83a41fb0
mark <mark.korput@gmail.com>
2019-04-08 15:58:19 +0200

    GameObjectAttr

---
+++ src/Id/Components/GameObjectAttr.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace MicroState.Id.Components
+{
+	public class GameObjectAttr : BaseAttr<GameObject>
+	{
+		[System.Serializable]
+		public class ValueTypeEvent : UnityEvent<GameObject> { }
+		public ValueTypeEvent ValueEvent = new ValueTypeEvent();
+
+		override protected void OnValue(GameObject v)
+		{
+			if (!(this.InvokeWhenInactive || this.isActiveAndEnabled)) return;
+			this.ValueEvent.Invoke(v);
+		}
+	}
+
+#if UNITY_EDITOR
+    [CustomEditor(typeof(GameObjectAttr))]
+    [CanEditMultipleObjects]
+    public class GameObjectAttrEditor : AttrRefEditor<GameObject, GameObjectAttr>
+    {
+
+    }
+#endif
+}
