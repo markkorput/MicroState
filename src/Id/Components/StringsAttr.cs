dd1fded0c07a91d22ac8c078827fc2694e2e68d3
mark <mark.korput@gmail.com>
2019-04-09 12:49:22 +0200

    StringsAttr

---
+++ src/Id/Components/StringsAttr.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace MicroState.Id.Components
+{
+	public class StringsAttr : BaseAttr<string[]>
+	{
+		[System.Serializable]
+		public class ValueTypeEvent : UnityEvent<string[]> { }
+		public ValueTypeEvent ValueEvent = new ValueTypeEvent();
+
+		override protected void OnValue(string[] v)
+		{
+			if (!(this.InvokeWhenInactive || this.isActiveAndEnabled)) return;
+			this.ValueEvent.Invoke(v);
+		}
+	}
+
+#if UNITY_EDITOR
+    [CustomEditor(typeof(StringsAttr))]
+    [CanEditMultipleObjects]
+    public class StringsAttrEditor : AttrRefEditor<string[], StringsAttr>
+    {
+
+    }
+#endif
+}
