c2ccac75b908445e3454ce46d10dcc203a01c2fc
mark <mark.korput@gmail.com>
2019-04-09 16:20:02 +0200

    ColorAttr

---
+++ src/Id/Components/ColorAttr.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace MicroState.Id.Components
+{
+	public class ColorAttr : BaseAttr<Color>
+	{
+		[System.Serializable]
+		public class ValueTypeEvent : UnityEvent<Color> { }
+		public ValueTypeEvent ValueEvent = new ValueTypeEvent();
+
+		override protected void OnValue(Color v)
+		{
+			if (!(this.InvokeWhenInactive || this.isActiveAndEnabled)) return;
+			this.ValueEvent.Invoke(v);
+		}
+	}
+
+#if UNITY_EDITOR
+    [CustomEditor(typeof(ColorAttr))]
+    [CanEditMultipleObjects]
+    public class ColorAttrEditor : AttrRefEditor<Color, ColorAttr>
+    {
+
+    }
+#endif
+}
